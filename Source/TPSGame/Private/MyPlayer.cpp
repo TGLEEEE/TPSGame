f5bc519f1071e83ee1960e59d16d2f11924d5fcc
TGLEEEE <121008880+TGLEEEE@users.noreply.github.com>
2023-01-12 18:28:25 +0900

    feat: 플레이어 에셋 및 이동구현

---
+++ Source/TPSGame/Private/MyPlayer.cpp
@@ -0,0 +1,96 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyPlayer.h"
+#include <GameFramework/SpringArmComponent.h>
+#include <Camera/CameraComponent.h>
+#include <GameFramework/CharacterMovementComponent.h>
+
+// Sets default values
+AMyPlayer::AMyPlayer()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	ConstructorHelpers::FObjectFinder<USkeletalMesh> mesh(TEXT("/Script/Engine.SkeletalMesh'/Game/Assets/player/character.character'"));
+	if (mesh.Succeeded())
+	{
+		GetMesh()->SetSkeletalMesh(mesh.Object);
+		GetMesh()->SetRelativeLocationAndRotation(FVector(0, 0, -90), FRotator(0, -90, 0));
+	}
+
+	playerSpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("playerSpringArm"));
+	playerSpringArm->SetupAttachment(RootComponent);
+	playerSpringArm->TargetArmLength = 250;
+	playerCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("playerCamera"));
+	playerCamera->SetupAttachment(playerSpringArm);
+	playerCamera->SetRelativeLocation(FVector(0, 50, 100));
+
+	bUseControllerRotationYaw = true;
+	playerSpringArm->bUsePawnControlRotation = true;
+	playerCamera->bUsePawnControlRotation = true;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+}
+
+// Called when the game starts or when spawned
+void AMyPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyPlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	// 월드기준 dir를 플레이어기준 dir로 변화하기 위해 플레이어 rotaion을 트랜스폼으로 만듬
+//	FTransform playerTrans = GetControlRotation();
+	FTransform playerTrans(GetControlRotation());
+	// 플레이어 트랜스폼에 맞게 dir 변환
+	FVector newDir = playerTrans.TransformVector(dir);
+	// Z값 제거 (바닥정지안하게)
+	newDir.Z = 0;
+	newDir.Normalize();
+	// 변환된 dir로 이동 (속도는 self에)
+	AddMovementInput(newDir);
+	
+}
+
+// Called to bind functionality to input
+void AMyPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction(TEXT("Jump"), IE_Pressed, this, &AMyPlayer::InputActionJump);
+	PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &AMyPlayer::InputAxisLookUp);
+	PlayerInputComponent->BindAxis(TEXT("TurnRight"), this, &AMyPlayer::InputAxisTurnRight);
+	PlayerInputComponent->BindAxis(TEXT("MoveVertical"), this, &AMyPlayer::InputAxisMoveVertical);
+	PlayerInputComponent->BindAxis(TEXT("MoveHorizontal"), this, &AMyPlayer::InputAxisMoveHorizontal);
+}
+
+void AMyPlayer::InputAxisLookUp(float value)
+{
+	AddControllerPitchInput(value);
+}
+
+void AMyPlayer::InputAxisTurnRight(float value)
+{
+	AddControllerYawInput(value);
+}
+
+void AMyPlayer::InputAxisMoveVertical(float value)
+{
+	dir.X = value;
+}
+
+void AMyPlayer::InputAxisMoveHorizontal(float value)
+{
+	dir.Y = value;
+}
+
+void AMyPlayer::InputActionJump()
+{
+	Jump();
+}
+
---
+++ Source/TPSGame/Public/MyPlayer.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MyPlayer.generated.h"
+
+UCLASS()
+class TPSGAME_API AMyPlayer : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AMyPlayer();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(EditDefaultsOnly)
+	class USpringArmComponent* playerSpringArm;
+	UPROPERTY(EditDefaultsOnly)
+	class UCameraComponent* playerCamera;
+
+	void InputAxisLookUp(float value);
+	void InputAxisTurnRight(float value);
+	void InputAxisMoveVertical(float value);
+	void InputAxisMoveHorizontal(float value);
+	void InputActionJump();
+
+private:
+	FVector dir;
+};
