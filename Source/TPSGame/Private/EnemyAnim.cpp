6d1426b7f3505aa16cf1d2b5eb63dd2b85478b27
TGLEEEE <121008880+TGLEEEE@users.noreply.github.com>
2023-02-07 12:52:02 +0900

    feat: enemy공격 완성
    오른팔에 boxcomp부착
    공격시 notify따라 콜리전 on and off

--- Source/TPSGame/Private/EnemyAnim.cpp
+++ Source/TPSGame/Private/EnemyAnim.cpp
@@ -6,7 +6,31 @@
 #include "Enemy.h"
 #include "Components/BoxComponent.h"
 
+void UEnemyAnim::NativeBeginPlay()
+{
+	Super::NativeBeginPlay();
+	me = Cast<AEnemy>(TryGetPawnOwner());
+}
+
 void UEnemyAnim::OnEndAttackAnimation()
 {
 	bAttackPlay = false;
+}
+
+void UEnemyAnim::AnimNotify_EnemyAttackStart()
+{
+	if (me)
+	{
+		me->boxComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+		UE_LOG(LogTemp, Warning, TEXT("collision on"));
+	}
+}
+
+void UEnemyAnim::AnimNotify_EnemyAttackEnd()
+{
+	if (me)
+	{
+		me->boxComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+		UE_LOG(LogTemp, Warning, TEXT("collision off"));
+	}
 }
