42ba48c21f3effb60a6c97ff4fd46f0dafefbd1d
Jellyforest <121009043+Jellyforest@users.noreply.github.com>
2023-02-09 09:39:25 +0900

    전전전류들이 기기기절하듯

--- Source/TPSGame/Private/RealElectricTrap.cpp
+++ Source/TPSGame/Private/RealElectricTrap.cpp
@@ -29,7 +29,7 @@ ARealElectricTrap::ARealElectricTrap()
 void ARealElectricTrap::BeginPlay()
 {
 	Super::BeginPlay();
-	boxComp->OnComponentEndOverlap.AddDynamic(this, &ARealElectricTrap::InRealElectricTrap);
+	boxComp->OnComponentBeginOverlap.AddDynamic(this, &ARealElectricTrap::InRealElectricTrap);
 
 	
 }
@@ -52,7 +52,7 @@ void ARealElectricTrap::Tick(float DeltaTime)
 	}
 }
 
-void ARealElectricTrap::InRealElectricTrap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+void ARealElectricTrap::InRealElectricTrap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	enemy = Cast<AEnemy>(OtherActor);
 	if (nullptr != enemy)
--- Source/TPSGame/Public/RealElectricTrap.h
+++ Source/TPSGame/Public/RealElectricTrap.h
@@ -31,7 +31,7 @@ public:
 		class UArrowComponent* arrow;
 
 	UFUNCTION(BlueprintCallable)
-	void InRealElectricTrap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	void InRealElectricTrap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 	UPROPERTY(EditAnywhere)
 	class UBoxComponent* boxComp;
